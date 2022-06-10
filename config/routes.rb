5b3781ac97eaa411c76c2121eb220b727b1b9684
johanleon14 <57290266+johanleon14@users.noreply.github.com>
2022-06-10 17:41:44 -0500

    Feature/add auth system (#5)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :tasks
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
 end
